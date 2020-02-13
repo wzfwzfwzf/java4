56483446d04c460ad76e1e14e86c224084a88b21
wzfwzfwzf <58935915+wzfwzfwzf@users.noreply.github.com>
2020-02-13 17:11:32 +0800

    str2

--- src/TestGit1.java
+++ src/TestGit1.java
@@ -2,6 +2,7 @@ public class TestGit1 {
     public static void main(String[] args) {
         int a=0;
         System.out.println(a);
-        String str="str1"
+        String str="str1";
+        String str2="str2";
     }
 }
